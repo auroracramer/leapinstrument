482977426218ef6ad416dc043be68b6873a1e5fd
Jason Cramer <jason.t.cramer@gmail.com>
2013-08-14 21:54:45 -0400

    implement basic instrument interface

--- src/main/scala/Instrument.scala
+++ src/main/scala/Instrument.scala
@@ -1,7 +1,31 @@
-package com.jasoncramer.instrument
+package com.jasoncramer.leapinstrument
 
-trait Instrument {
-  def setFreq(freq: Double): Unit = ???
-  def setVolume(vol: Double): Unit = ???
-  def setVelocity(vel: Int): Unit = ???
+import com.jsyn._
+import com.jsyn.unitgen.UnitOscillator
+import com.jasoncramer.scsyn._
+
+
+class Instrument(osc: UnitOscillator) {
+  val synth = JSyn.createSynthesizer()
+  val lineOut = LineOut()
+  synth.add(osc)
+  synth.add(lineOut)
+  osc.output.connect(0, lineOut.input, 0)
+  osc.output.connect(0, lineOut.input, 1)
+  osc.amplitude.set(0.1)
+
+  def start(): Unit = {
+    lineOut.start()
+    synth.start()
+  }
+
+  def stop(): Unit = {
+    lineOut.stop()
+    synth.stop()
+  }
+
+  def setFreq(freq: Double): Unit = osc.frequency.set(freq)
+  def setVolume(vol: Double): Unit = osc.amplitude.set(vol)
 }
+
+case class SawtoothInstrument() extends Instrument(SawtoothOscillator())
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,49 +1,23 @@
-import de.sciss.synth._
-import ugen._
+package com.jasoncramer.leapinstrument
+
+import com.jsyn._
+import com.jasoncramer.scsyn._
 
 object Main extends App {
-  val cfg = Server.Config()
-  cfg.programPath = "/usr/bin/scsynth"
+  val instr = SawtoothInstrument()
+  instr.start()
 
-  Server.run(cfg) { s =>
-    TestInstrument(TestUpdateX(), TestUpdateY())
-  }
-}
-
-object TestInstrument {
-  def apply(x: UpdatingX, y: UpdatingY) = {
-    play {
-      // harmonic series
-      val pitch = Vector(50, 53.86, 57.02, 59.69, 62, 64.04, 65.86, 67.51, 69.02, 71.69, 72.88, 74)
-      val mousex = x.value
-      val mousey = y.value
-      val triggerSpacing = 0.5 / (pitch.size - 1)
-      val panSpacing = 1.5 / (pitch.size - 1)
-      val out = Mix.tabulate(pitch.size) { i =>
-        // place trigger points from 0.25 to 0.75
-        val trigger = HPZ1.kr(mousex > (0.25 + (i * triggerSpacing))).abs
-        val pluck = PinkNoise.ar(Decay.kr(trigger, 0.05))
-        val period = pitch(i).midicps.reciprocal
-        val string = CombL.ar(pluck, period, period, 8)
-        Pan2.ar(string, i * panSpacing - 0.75)
-      }
-      LeakDC.ar(out)
+  while(true) {
+    val ln = readLine(">>> ").split(" ")
+    assert(ln.length <= 2)
+    ln(0) match {
+      case "freq" => instr.setFreq(ln(1).toDouble)
+      case "vol" => instr.setVolume(ln(1).toDouble)
+      case "exit" =>
+        instr.stop()
+        System.exit(0)
+      case _ => {}
     }
   }
 }
 
-trait UpdatingX {
-  def value: de.sciss.synth.ugen.MouseX
-}
-
-trait UpdatingY {
-  def value: de.sciss.synth.ugen.MouseY
-}
-
-case class TestUpdateX extends UpdatingX {
-  def value = MouseX(0.0, 1.0, 0.0, 0.2)
-}
-
-case class TestUpdateY extends UpdatingY{
-  def value = MouseY.kr
-}
